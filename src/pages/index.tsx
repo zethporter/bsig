94bb344c02e9f337ea4d4313c4b5357716a9ca15
Zeth <zeth@bmisw.com>
2023-06-01 13:04:24 -0600

    fixed hero scrolling from title box to info box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ const Pictures = () => {
       {imageEndings.map((ending) => (
         <div className="carousel-item m-2" key={ending}>
           <img
-            className="h-screen rounded-box"
+            className="rounded-box h-screen"
             src={`https://bsig.s3.us-east-2.amazonaws.com/ftn/freeTheNip${ending}.jpg`}
             alt={ending}
           />
@@ -53,9 +53,9 @@ const Home: NextPage = () => {
             }}
           >
             <div className="hero-overlay bg-opacity-60"></div>
-            <div className="hero-content text-center text-neutral-content">
-              <div className="max-w-md">
-                <h1 className="font-serif text-5xl text-white sm:text-[5rem]">
+            <div className="h-screen w-full overflow-auto p-5">
+              <div className="h-screen w-full text-center">
+                <h1 className="mx-auto mt-56 w-2/3 font-serif text-5xl text-white sm:text-[5rem]">
                   Brayden Singley is{" "}
                   <span className={clsx("text-accent", pacifico.className)}>
                     G
@@ -67,6 +67,18 @@ const Home: NextPage = () => {
                     Y
                   </span>
                 </h1>
+              </div>
+              <div className="mx-auto w-3/4 pb-72 text-center">
+                <p className="inline-block align-middle font-serif text-lg text-white sm:text-3xl">
+                  Brayden is a gay dancer. Currently Living in New York City.
+                  Born and raised in Utah he moved to the big apple to follow
+                  his dream of being a professional performer. He loves to
+                  Express himself through dance. He is most passionate about
+                  contemporary and gaga. He is a proud gay boy and loves
+                  exploring th diverse and vibrant culture of New York city.
+                  Bayden is a friendly, outgoing and creative person who always
+                  has a smile on his face.
+                </p>
               </div>
             </div>
           </div>
